f6a8caefe70fd577a9d1aaaa278e6c25d4bac2aa
Idonotus <102253332+Idonotus@users.noreply.github.com>
2025-12-07 20:04:02 +0000

    feat: what could go wrong adding dynamic parameters to a type safe language?

--- src/rule.rs
+++ src/rule.rs
@@ -1,3 +1,45 @@
+use std::mem::ManuallyDrop;
+
+struct Player {
+
+}
+
+struct Card {
+
+}
+
+union GameVar {
+	num: i32,
+	player: ManuallyDrop<Player>,
+	card: ManuallyDrop<Card>
+}
+
+struct TypeContainer {
+	game_type: GameType,
+	var: GameVar
+}
+
+#[derive(PartialEq, Eq)]
+enum GameType {
+	Number,
+	Player,
+	Card,
+}
+
+impl TypeContainer {
+	fn is_safe(&self, expected_type: GameType) -> bool {
+		self.game_type == expected_type
+	}
+}
+
 pub trait Rule {
 	fn get_text() -> String;
+}
+
+type ActionContext = Vec<GameVar>;
+
+pub trait Action {
+	fn is_expected_context_safe(&self, context: Vec<GameType>) -> bool;
+	fn is_context_safe(&self, action_context: ActionContext) -> bool;
+	fn run_action(&self, action_context: ActionContext);
 }
